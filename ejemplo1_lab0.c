bc11158030f2076c66aba8a8ed52d23dc588adf8
Nicobelicko <63007466+Nicobelicko@users.noreply.github.com>
2021-02-02 23:19:30 -0500

    Add files via upload

---
+++ ejemplo1_lab0.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+char nombre [50];
+
+int main(){
+	/*int pid = getpid();
+	printf("Ingrese su nombre por favor: ");
+	scanf("%s",nombre);
+	printf("Hola %s, su id de proceso es %d", nombre, pid);*/
+	printf("Hola Mundo\n");
+	return 0;
+}

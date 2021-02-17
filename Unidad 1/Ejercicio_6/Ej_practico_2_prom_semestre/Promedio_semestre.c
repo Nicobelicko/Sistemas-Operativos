052ca36bb4c51d9564d0d9362c67218b13c83b4b
Nicolas <juann.ruiz@upb.edu.co>
2021-02-17 14:50:36 -0500

    Avance en promedio ponderado, ya estan los datos listos, solo falta escribirlos en el archivo de salida

--- Unidad 1/Ejercicio_6/Ej_practico_2_prom_semestre/Promedio_semestre.c
+++ Unidad 1/Ejercicio_6/Ej_practico_2_prom_semestre/Promedio_semestre.c
@@ -3,26 +3,29 @@
 #include <string.h>
 
 char FILE_TO_READ[] = "datos_lectura.txt";
-char *concat(const char *s1, const char *s2);
 
 int main()
 {
-    //printf("llega hasta 0");
-    int num_materias, won, lost, num_creditos;
-    double promedio, promedio_ponderado;
+
+    int num_materias = 0, won = 0, lost = 0;
+    double promedio = 0, promedio_ponderado = 0;
 
     char linea;
-    //printf("llega hasta 1");
+    char *linea1;
 
-    int won, lost;
-    double promedio;
-    char nota[4];
-    char creditos[1];
+    //char materia[30];
+    //char nota[3];
+    //char creditos[1];
+    char almacenador[40];
+    double nota=0.0;
+    double creditos=0.0;
+    int tot_creditos=0;
     int counter = 1;
+    int counter2=1;
     FILE *read, *write;
 
     read = fopen(FILE_TO_READ, "r");
-    write = fopen("informe_semestre.txt", "r+");
+    write = fopen("informe.txt", "r+");
     if (read == NULL)
     {
         printf("Error al abrir el archivo: %s", FILE_TO_READ);
@@ -35,11 +38,12 @@ int main()
 
         if (linea == '\n')
         {
-
             if (counter == 3)
             {
                 if (feof(read))
                     break;
+                num_materias++;
+                fputc(' ', write);
                 fputc(' ', write);
             }
             else
@@ -63,17 +67,56 @@ int main()
 
         fputc(linea, write);
     }
+    
+    read = fopen(FILE_TO_READ, "r");
+    int i=0;
+    while (i<num_materias)
+    {
+        
 
+        if (feof(read))
+            break;
+        
+        fgets(almacenador,40,read);
 
+        if(counter2==1){
+            printf("Materia %s",almacenador);
+            
+        }
+        if(counter2==2){
+            printf("nota %s",almacenador);
+            if((atof(almacenador)-3.0)<0){
+                lost+=1;
+            }else{
+                won+=1;
+            }
+            nota = atof(almacenador);
+        }
+        if(counter2==3){
+            printf("creditos %s",almacenador);
+            creditos=atof(almacenador);
+            tot_creditos+=(int)creditos;
+            counter2=0;
+            i++;
+            promedio+=nota*creditos;
+            printf("Promedio %3.1lf\n",promedio);
+        }
+        
+        
+        counter2++;
+        
 
+        
+       
+        
+        if(feof(read)) break;
+        
+        
+    }
+    printf("Ganadas: %d\nPerdidas: %d\n", won, lost);
+    printf("Total de creditos: %d\n",tot_creditos);
+    printf("Promedio ponderado: %3.4lf",promedio/tot_creditos);
     fclose(read);
     fclose(write);
 }
-char *concat(const char *s1, const char *s2)
-{
-    char *result = malloc(strlen(s1) + strlen(s2) + 1); // +1 for the null-terminator
-    // in real code you would check for errors in malloc here
-    strcpy(result, s1);
-    strcat(result, s2);
-    return result;
-}
+    

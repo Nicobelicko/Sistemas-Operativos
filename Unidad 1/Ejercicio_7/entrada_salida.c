fe8b24db756891c8d24d7e8584043d9e2a618ad1
Nicolas <juann.ruiz@upb.edu.co>
2021-02-19 08:15:28 -0500

    Ingreso codigos de ejercicios de solo preguntas.

---
+++ Unidad 1/Ejercicio_7/entrada_salida.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    char key;
+    printf("Prueba a scanf. Ingrese el numero 325 y presione ENTER:\n");
+    scanf("%d",&num);
+    printf("Ingrese cualquier tecla para terminar y presione ENTER:\n");
+    scanf("%c",&key);
+
+    return 0;
+}
---
+++ Unidad 1/Ejercicio_8/entrada_salida.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 10;
+    printf("enter a number: ");
+    scanf("%d", &a);
+    printf("You entered %d.\n", a);
+}

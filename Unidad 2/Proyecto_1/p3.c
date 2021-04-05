742616743afbd9b6f6ac670d77db312c8f31fb4c
Nicolas <juann.ruiz@upb.edu.co>
2021-04-04 23:53:03 -0500

    p3 listo

---
+++ Unidad 2/Proyecto_1/p3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <syscall.h>
+#include <sys/wait.h>
+int main(int argc, char *argv[])
+{
+
+    __pid_t pid_hijo;
+    
+
+    for (int i = 0; i < 2; i++)
+    {
+        pid_hijo = fork();
+        if (pid_hijo == 0)
+        {
+            if (i == 0) // minimo
+            {
+                execvp("./p1", argv);
+            }
+            else if (i == 1)
+            { //maximo
+
+                execvp("./p2", argv);
+            }
+           
+        }
+        else
+        {
+            wait(NULL);
+        }
+    }
+
+    return 0;
+}
